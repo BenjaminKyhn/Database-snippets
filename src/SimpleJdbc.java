ca9141dd1720270b338bbe1d585413a0bff21ea3
Benjamin Kyhn <benj2605@edu.easj.dk>
2019-11-07 12:42:18 +0100

    SimpelJdbc tilpasset mobilepay databasen

--- src/SimpleJdbc.java
+++ src/SimpleJdbc.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
 public class SimpleJdbc {
     public static void main(String[] args) throws SQLException {
 
-        System.out.println("\n****** \nProgram til hentning af landedata fra world.sql databasen. \n****** ");
+        System.out.println("\n****** \nOpslagsværk til mobilepays database. \n****** ");
 
         // #1. Connect to the database
         Connection connection = null;
         connection = DriverManager.getConnection(
-                "jdbc:mysql://localhost/world?serverTimezone=UTC",
+                "jdbc:mysql://localhost/mobilepay?serverTimezone=UTC",
                 "root",
                 "CodeWarrior8"
         );
@@ -18,19 +18,19 @@ public class SimpleJdbc {
 
         // #2. Create a statement
 
-        // ** Med Scanner kan vi få brugeren til at indtaste, hvilket land, de vil se info om
-        String landeNavn = "";
+        // ** Med Scanner kan vi få brugeren til at indtaste, hvilken person, de vil se info om
+        String brugerNavn = "";
 
         //Fortsæt med at spørge, indtil du skriver "exit"
-        while (!landeNavn.equalsIgnoreCase("exit")) {
+        while (!brugerNavn.equalsIgnoreCase("exit")) {
             Scanner scanner = new Scanner(System.in);
-            System.out.println("\nHvilket land vil du se oplysninger om? (Skriv 'exit' for at afslutte): ");
-            landeNavn = scanner.nextLine();
+            System.out.println("\nHvilken bruger vil du se oplysninger om? (Skriv 'exit' for at afslutte): ");
+            brugerNavn = scanner.nextLine();
 
-            String mitQuery = "SELECT * FROM world.country WHERE name LIKE  '" + '%' + landeNavn + '%' + "';";
-            //String mitQuery = "SELECT * FROM world.country WHERE name LIKE  '" + landeNavn + "';";
-            // ** Uden scanner kan vi kun vise det land, som vi hardcoder i query-et
-            //String mitQuery = "SELECT * FROM world.country WHERE name LIKE 'D%';";
+            String mitQuery = "SELECT * FROM brugere WHERE fornavn LIKE  '" + '%' + brugerNavn + '%' + "';";
+            //String mitQuery = "SELECT * FROM bruger WHERE fornavn LIKE  '" + brugerNavn + "';";
+            // ** Uden scanner kan vi kun vise den bruger, som vi hardcoder i query-et
+            //String mitQuery = "SELECT * FROM bruger WHERE fornavn LIKE 'D%';";
             Statement statement = connection.createStatement();
 
             // #3. Execute the statement and send the SQL-query to the SQL-server
@@ -43,10 +43,9 @@ public class SimpleJdbc {
             // #4. Iterate through the result and print the results (vi har måske flere resultater end 1, derfor vil en løkke læse alle rækker ud fra resultSettet)
             while (resultSet.next())
                 System.out.println(resultSet.getString(1) + " " +
-                        resultSet.getString(2) + ", " +
-                        resultSet.getString(3) + ", " +
-                        "Population: " +
-                        resultSet.getString("Population"));
+                        resultSet.getString(2) + " " +
+                        resultSet.getString(3) + " " +
+                        resultSet.getString(4) + " ");
         }
         // #5. Close the connection (always!)
         connection.close();
